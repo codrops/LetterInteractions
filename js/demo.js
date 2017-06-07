fce2cc139afcf5004dd04bd9426189e83661d0be
botelho <botelho@users.noreply.github.com>
2017-06-07 19:12:51 +0100

    Removed console.log

--- js/demo.js
+++ js/demo.js
@@ -13,7 +13,6 @@
 	const navdemos = document.querySelectorAll('nav.demos > .demo');
 	Array.from(navdemos).forEach(link => link.addEventListener('click', (ev) => {
 		ev.preventDefault();
-		console.log(ev.target.href);
 		document.body.classList.remove('render');
 		document.body.addEventListener('transitionend', () => window.location = ev.target.href);
 	}));
